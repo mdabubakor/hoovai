a46a9d13af0ac5fc6ba23df9389506ee765adbed
abubakar460 <mdsimul5060@gmail.com>
2024-02-24 11:55:22 -0800

    second

---
+++ data.ts
@@ -0,0 +1,6 @@
+export const Navbar =[
+    {title: 'Home', href:'/',id_:101},
+    {title: 'About', href:'/about',id_:102},
+    {title: 'Projects', href:'/project',id_:103},
+    {title: 'contact', href:'/contact',id_:104},
+]
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Container from './Container'
+import { Navbar } from '../../data'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <div>
-      Header
+      <Container >
+        <div>
+          <div>
+          <h1>Logo</h1>
+          </div>
+          <div>
+          {
+            Navbar.map((item)=>(
+              <ul key={item?.id_}>
+                <Link href={item?.href}>
+                <li>
+                  {item?.title}
+                </li>
+                </Link>
+              </ul>
+            ))
+          }
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
