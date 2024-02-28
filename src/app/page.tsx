2642d49da683756e02af527be27ed632c4c2e77f
abubakar460 <mdsimul5060@gmail.com>
2024-02-28 00:10:07 -0800

    dor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Banner from "@/Components/Banner";
+import Container from "@/Components/Container";
 import Header from "@/Components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <Container>
         <Banner />
-    </div>
+    </Container>
   );
 }
