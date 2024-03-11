c810907470062c27d00931f97a9a359c25e7d4e4
abubakar460 <mdsimul5060@gmail.com>
2024-03-11 10:49:10 -0700

    hoy

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -22,7 +22,7 @@ const About = () => {
               <div className="grid items-center justify-center   mt-20">
          <Image src={AB} alt="banner" className="relative "  />
          {/* <div className=" bg-yellow-200 h-40 w-2 ms-40 absolute bottom-[319px] left-24  shadow-inner backdrop-blur-sm   shadow-yellow-700"/> */}
-         <div className=" bg-yellow-300 h-5 w-[100px]   top-[789px] left-[160px]      absolute  rounded-md  shadow-inner   shadow-yellow-700"/>
+         <div className=" bg-yellow-300 h-5 w-[100px]  sm:left-[200px] top-[789px] lg:left-[160px]      absolute  rounded-md  shadow-inner   shadow-yellow-700"/>
          <div className=" bg-yellow-300 h-4 w-[275px] ms-40 shadow-inner shadow-yellow-700 rounded-md "/>
         </div>
               </div>
